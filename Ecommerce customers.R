c00adda854a71ecbd627f45526c9f26f41eee398
Amal-Almutairy <71967687+Amal-Almutairy@users.noreply.github.com>
2020-11-05 16:12:32 +0300

    update

---
+++ Ecommerce customers.R
@@ -0,0 +1,27 @@
+# Ecommerce customers analysis
+# 03.11.2020
+# Amal Almutairy
+library(tidyverse)
+library(rio)
+library(dplyr)
+library(ggplot2)
+library(rsample)
+library(recipes)
+library(caret)
+
+# Data Source: https://www.kaggle.com/volpatto/coffee-quality-database-from-cqi
+
+# Ecommerce customers database:
+View(Ecommerce_customers)
+library(readr)
+Ecommerce_customers <- read_csv("Ecommerce_customers.csv")
+
+EC(ls)
+
+
+selections
+
+Methods for Select Variables in Step Functions
+
+has_role() all_predictors() all_outcomes() has_type() all_numeric() all_nominal() current_info()
+
